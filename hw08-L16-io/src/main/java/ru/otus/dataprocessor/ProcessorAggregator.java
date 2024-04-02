17f1b09bfc8d932fd9c4057a58606476795b886b
Елизавета Рюмшина <liza.rum@list.ru>
2024-04-02 10:27:28 +0500

    Used stream for grouping inside ProcessorAggregator

--- hw08-L16-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-L16-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -1,6 +1,7 @@
 package ru.otus.dataprocessor;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import ru.otus.model.Measurement;
 
@@ -9,13 +10,7 @@ public class ProcessorAggregator implements Processor {
     @Override
     public Map<String, Double> process(List<Measurement> data) {
         // группирует выходящий список по name, при этом суммирует поля value
-        List<Measurement> sortedList = new ArrayList<>(data);
-        sortedList.sort(Comparator.comparing(Measurement::name));
-
-        Map<String, Double> result = new TreeMap<>();
-        for (Measurement m : sortedList) {
-            result.put(m.name(), result.getOrDefault(m.name(), 0.0) + m.value());
-        }
-        return result;
+        return data.stream()
+                .collect(Collectors.groupingBy(Measurement::name,TreeMap::new, Collectors.summingDouble(Measurement::value)));
     }
 }

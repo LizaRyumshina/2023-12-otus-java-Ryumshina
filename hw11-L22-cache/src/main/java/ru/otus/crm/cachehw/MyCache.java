b15790cdf76da3d39447b901af1161f4d7229065
Елизавета Рюмшина <liza.rum@list.ru>
2024-04-21 18:00:16 +0500

    Implemented MyCache class. And the class added for client caching in DbServiceClientImpl

--- hw11-L22-cache/src/main/java/ru/otus/crm/cachehw/MyCache.java
+++ hw11-L22-cache/src/main/java/ru/otus/crm/cachehw/MyCache.java
@@ -1,30 +1,45 @@
 package ru.otus.crm.cachehw;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MyCache<K, V> implements HwCache<K, V> {
-    // Надо реализовать эти методы
-
+    private Map<K, V> cache = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        notify(key, value, "put");
     }
-
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = cache.get(key);
+        notify(key, value, "remove");
+        cache.remove(key);
     }
 
     @Override
-    public V get(K key) {
-        throw new UnsupportedOperationException();
+    public V get(K key){
+        V value = cache.get(key);
+        System.out.println("Size of cache: " + cache.size() + ", get key=" + key + " value=" + value);
+        notify(key, value, "get");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
+    }
+    public void notify(K key, V value, String action) {
+        for (HwListener<K, V> listener : listeners) {
+            listener.notify(key, value, action);
+        }
     }
 }
--- hw11-L22-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw11-L22-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionManager;
+import ru.otus.crm.cachehw.MyCache;
 import ru.otus.crm.model.Client;
 
 public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+    private MyCache<String, Client> cacheClients = new MyCache<>();
 
     private final DataTemplate<Client> clientDataTemplate;
     private final TransactionManager transactionManager;
@@ -29,6 +31,7 @@ public class DbServiceClientImpl implements DBServiceClient {
                 return savedClient;
             }
             var savedClient = clientDataTemplate.update(session, clientCloned);
+            cacheClients.put(String.valueOf(clientCloned.getId()), clientCloned);
             log.info("updated client: {}", savedClient);
             return savedClient;
         });
@@ -36,11 +39,17 @@ public class DbServiceClientImpl implements DBServiceClient {
 
     @Override
     public Optional<Client> getClient(long id) {
-        return transactionManager.doInReadOnlyTransaction(session -> {
-            var clientOptional = clientDataTemplate.findById(session, id);
-            log.info("client: {}", clientOptional);
-            return clientOptional;
-        });
+        Optional<Client> client = Optional.ofNullable(cacheClients.get(String.valueOf(id)));
+        if (client.isEmpty()){
+            client = transactionManager.doInReadOnlyTransaction(session -> {
+                var clientOptional = clientDataTemplate.findById(session, id);
+                log.info("client: {}", clientOptional);
+                cacheClients.put(String.valueOf(id), clientOptional.get().clone());
+                return clientOptional;
+            });
+            cacheClients.put(String.valueOf(client.get().getId()), client.get().clone());
+        }
+        return client;
     }
 
     @Override
